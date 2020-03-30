e65bd875fa56c8ca1672cd4ad99fdd1976579c55
KUCZYN\kaczk <kaczksa>
2020-03-30 20:01:11 +0200

    Giera

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private Transform bullet;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        bullet.position += Vector3.up * speed;
+
+        if (bullet.position.y >= 7)
+            Destroy(bullet.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            PlayerScore.playerScore += 10;
+        }
+        else if (other.tag == "Base")
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemyBulletController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour
+{
+    private Transform bullet;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        bullet.position += Vector3.down * speed;
+
+        if (bullet.position.y <= -7)
+            Destroy(bullet.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            EnemyController enemy = new EnemyController();
+            Destroy(enemy.enemyHolder);
+            GameOver.isPlayerDead = true;
+        }
+        else if (other.tag == "Base")
+        {
+            GameObject playerBase = other.gameObject;
+            BaseHealth baseHealth = playerBase.GetComponent<BaseHealth>();
+            baseHealth.health -= 1;
+            Destroy(gameObject);
+        }
+    }
+}
